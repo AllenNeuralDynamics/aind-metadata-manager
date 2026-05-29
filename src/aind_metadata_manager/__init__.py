638da6a8ce639c0db6833df129769ad7bfbdb664
Arielle Leon <ariellel@alleninstitute.org>
2026-05-29 14:28:53 -0700

    feat: expose MetadataManager and MetadataSettings from package root

--- src/aind_metadata_manager/__init__.py
+++ src/aind_metadata_manager/__init__.py
@@ -1,3 +1,10 @@
 """Init package"""
 
+from aind_metadata_manager.metadata_manager import (
+    MetadataManager,
+    MetadataSettings,
+)
+
 __version__ = "0.0.5"
+
+__all__ = ["MetadataManager", "MetadataSettings"]
